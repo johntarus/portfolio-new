d80829833f33cb72539dc693dbe99040b306608c
user name <user email>
2021-02-11 11:39:35 +0300

    commit on 'main'

--- contact.php
+++ contact.php
@@ -1,16 +1,22 @@
 <?php
-if (isset($_POST['submit'])) {
     $name = $_POST['name'];
-    $subject = $_POST['subject'];
-    $mailFrom = $_POST['mail'];
+    $visitor_email = $_POST['email'];
     $message = $_POST['message'];
-    
-    
-    $mailTo = "tarusjohn96@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from ".$name.".\n\n".$message;
-    
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: index.php?mailsend");
-    }
+
+    $_email_from = 'nicksontarus91@gmail.com';
+    $_email_subject = 'enquiry';
+
+    $_email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                        "User Message: $message.\.".;
+
+    $to = "tarusjohn96@gmail.com";
+    $headers = "From: $_email_from \r\n";
+
+    $headers = "Reply-To: $visitor_email \r\n";
+
+    $mail($to,$_email_subject,$_email_body,$headers);
+
+    header(Location: index.html);
+
 ?>
